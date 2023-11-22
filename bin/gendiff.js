88f4faae1d3a58f4a50cb50198add0c752a1ba48
Palina Chuiko <palinachuiko@Palinas-MacBook-Air.local>
2023-11-22 10:22:15 +0100

    nested structure

---
+++ bin/gendiff.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+import { program } from 'commander';
+import genDiff from '../src/index.js';
+
+program
+  .description('Compares two configuration files and shows a difference.')
+  .option('-V, --version', 'output the version number')
+  .option('-f, --format <type> ', 'output format')
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
+    console.log(genDiff(filepath1, filepath2));
+  })
+  .parse(process.argv);
